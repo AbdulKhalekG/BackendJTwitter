dc2e46f8fe18415a29f09a589e7bb3da1ea0e3f2
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2022-11-28 11:22:04 -0400

    update

--- src/database.js
+++ src/database.js
@@ -21,10 +21,10 @@ const pool = new Pool(config);
 //funcion create user
 const createuser= async (req,res) =>{
 
-    const{ nombre, username, correo, bio, direccion, birthday, clave} = req.body;
+    const{ nombre, username, correo, bio, birthday, clave} = req.body;
     const passwordencriptado = await helpers.encryptPassword(clave)
-    const response = await pool.query('INSERT INTO usuarios (nombre, username, correo, bio, direccion, birthday, clave) VALUES($1, $2, $3, $4, $5, $6, $7)', [
-        nombre, username, correo, bio, direccion, birthday, passwordencriptado])
+    const response = await pool.query('INSERT INTO usuarios (nombre, username, correo, bio, birthday, clave) VALUES($1, $2, $3, $4, $5, $6)', [
+        nombre, username, correo, bio, birthday, passwordencriptado])
     console.log(response);
     res.json(response.rows)
 }
@@ -32,8 +32,8 @@ const createuser= async (req,res) =>{
 //funcion modify user
 const modifyuser=async (req,res)=>{
 
-    const{ nombre, username, correo, bio, direccion, birthday, clave,id_usuario} = req.body;
-    const response = await pool.query('UPDATE usuarios SET nombre= $1 username= $2, correo=$3, bio =$4, direccion=$5, birthday=$6, clave=$7 WHERE id_usuario=$8', [nombre, username, correo, bio, direccion, birthday,clave,id_usuario])
+    const{ nombre, username, correo, bio, birthday, clave,id_usuario} = req.body;
+    const response = await pool.query('UPDATE usuarios SET nombre= $1 username= $2, correo=$3, bio =$4, birthday=$5, clave=$6 WHERE id_usuario=$7', [nombre, username, correo, bio, birthday,clave,id_usuario])
     console.log(response);
     res.json(response.rows)
 

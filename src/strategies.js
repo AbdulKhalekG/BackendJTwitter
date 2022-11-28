07a46887e70e90c77e256645f8131f70ebe57e56
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2022-11-28 12:05:18 -0400

    update

--- src/strategies.js
+++ src/strategies.js
@@ -4,11 +4,11 @@ const { Pool } = require('pg');
 const helpers =require('./helpers')
 
 const config={
-  connectionStreing: process.env.DATABASE_URL,
+  
+  connectionString: process.env.DATABASE_URL,
   max:500,
   min:100,
-  ssl: {rejectUnauthorized:false}
- 
+  ssl:{rejectUnauthorized:false}
 };
   
   const pool = new Pool(config); 
@@ -28,15 +28,12 @@ const config={
       const result= await pool.query('SELECT* FROM usuarios WHERE username=$1',[user.username])
       if(result.rows.length>0){
          const newuser =result.rows[0];
-       
-         console.log(newuser.clave);
-         const validpassword= await helpers.compararclave(password,newuser.clave) 
+         const validpassword= await helpers.compararclave(user.clave,newuser.clave) 
         
          if(validpassword){
-          //password===newuser.clave
+          
           done(null,newuser,console.log('welcome'))
           user.id=newuser.id_usuario
-         
           passport.serializeUser((user,done)=>{
             done(null,user.id)
           })
@@ -47,7 +44,7 @@ const config={
               
          }
       }else{
-        return done(null, false,console.log('user does not exist'))   
+        return done(null, false,console.log('user no exist'))   
         
       }
       
@@ -59,11 +56,6 @@ const config={
 );
 
 
-
-passport.deserializeUser( async (id_usuario,done)=>{
-    const rows = await pool.query('SELECT * FROM usuario WHERE id_usuario=?',[id_usuario])
-    done(null,rows[0])
-})
 
 module.exports={
   LocalStrategy

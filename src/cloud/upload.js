8d2260388ee4d9cf5caa5355978b62782275436d
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2022-12-02 16:26:29 -0400

    update

---
+++ src/cloud/upload.js
@@ -0,0 +1,9 @@
+const cloud = require('cloudinary').v2
+
+cloud.config({ 
+    cloud_name: 'do8qt8elf', 
+    api_key: '688516197556968', 
+    api_secret: 'aFzNYToU9utfRKZQA1r6yarg-TE' 
+  });
+
+module.exports = cloud
